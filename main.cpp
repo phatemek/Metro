ce3c55263a7ced7d18dc5ed399b815ffbd78e687
Shahriar-0 <attarshahriar@gmail.com>
2022-07-30 09:48:21 -0400

    added time approximation

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <float.h>
 #include <math.h>
 #include <vector>
 
 using namespace std;
 
 const string METRO_STATIONS_FILE = "./MetroStations.csv";
+constexpr double EARTH_RADIUS = 6371;
+constexpr double CONVERTER = 2156.33;
 
 class MetroStation {
 private:
@@ -16,20 +19,17 @@ private:
     double longitude;
 public:
     MetroStation(string _name, double _latitude, double _longitude);
-    string getName();
-    double calcDistance(double x, double y);
+    string get_name();
+    double calc_distance(double x, double y);
 };
 
 MetroStation::MetroStation(string _name, double _latitude, double _longitude)
     : name(_name), latitude(_latitude), longitude(_longitude) {}
 
-string MetroStation::getName() { return name; }
+string MetroStation::get_name() { return name; }
 
-double MetroStation::calcDistance(double x, double y) {
-    return sqrt(pow((latitude - x), 2) + pow((longitude - y), 2));
-}
 
-vector<MetroStation*> addLocations() {
+vector<MetroStation*> add_locations() {
     vector<MetroStation*> metroStations;
     string name;
     double latitude, longitude;
@@ -42,10 +42,29 @@ vector<MetroStation*> addLocations() {
     return metroStations;
 }
 
+double to_radians( double degree) { return ((M_PI) * degree / 180); }
+
+double MetroStation::calc_distance(double x, double y) {
+    return sqrt(pow((latitude - x), 2) + pow((longitude - y), 2));
+}
+
+string calc_time(double distance) {
+    double timeInMinute = distance * CONVERTER;
+    int minute, second;
+    minute = timeInMinute;
+    second = (timeInMinute - minute) * 60;
+    string sMinute = to_string(minute), sSecond = to_string(second);
+    if (minute < 10)
+        sMinute = "0" + sMinute;
+    if (second < 10)
+        sSecond = "0" + sSecond;
+    return sMinute + ":" + sSecond;
+}
+
 void run(const vector<MetroStation*>& metroStations) {
 
     while(true) {
-        double latitude, longitude, distance, shortestDistance = 10;
+        double latitude, longitude, distance, shortestDistance = DBL_MAX;
         MetroStation* closestStation = NULL;
         cout << "Latitude: ";
         cin >> latitude;
@@ -53,14 +72,16 @@ void run(const vector<MetroStation*>& metroStations) {
         cin >> longitude;
 
         for(auto& station : metroStations) {
-            distance = station->calcDistance(latitude, longitude);
+            distance = station->calc_distance(latitude, longitude);
             if(distance < shortestDistance){
                 shortestDistance = distance;
                 closestStation = station;
             }
         }
 
-        cout << "Closest Station: " << closestStation->getName();
+
+        cout << "Closest station: " << closestStation->get_name() << 
+                ", and the approximate time is(if you are walking):" << calc_time(shortestDistance);
         cout << endl << "**************************************************************" << endl;
     }
 }
@@ -71,7 +92,7 @@ void clean_vector(vector<MetroStation*>& metroStations){
 }
 
 int main() {
-    vector<MetroStation*> metroStations = addLocations();
+    vector<MetroStation*> metroStations = add_locations();
     run(metroStations);
     clean_vector(metroStations);
     return 0;
